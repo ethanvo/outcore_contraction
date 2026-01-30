aa0ea14f10883dfa9a1838c7935db49f226ee5c6
ethanvo <ethan.vo@hotmail.com>
2026-01-30 10:45:23 -0500

    Move test_tensor_store.c to tests directory and update CMakeLists.txt
    This commit moves the test_tensor_store.c file from the root directory to the tests directory and updates the CMakeLists.txt file to reference the new location. Also fixed include paths in test file.

--- tests/test_tensor_store.c
+++ tests/test_tensor_store.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "include/tensor_store.h"
+#include <hdf5.h>
+#include "tensor_store.h"
 
 int main() {
     printf("Testing tensor_store.c functions...\n");
